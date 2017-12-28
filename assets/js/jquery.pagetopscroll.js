1247b9d781c5e066944b2dab839964b6175b0a1b
福島 正貴 <fu.webdesign@gmail.com>
2017-12-28 10:10:03 +0900

    initial commit

--- assets/js/jquery.pagetopscroll.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	var btnPagetop = $('#page-top');
-    btnPagetop.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
-});
